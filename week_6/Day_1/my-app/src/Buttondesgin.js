857c69fd03721bc5f39b7865e697563b98bbbeb4
Richa-12y <kricha000@gmail.com>
2021-11-14 21:04:09 +0530

    object deforamtion

--- week_6/Day_1/my-app/src/Buttondesgin.js
+++ week_6/Day_1/my-app/src/Buttondesgin.js
@@ -1,11 +1,26 @@
 import React from 'react'
 //buttonoverwriteBackgroundColor,buttonText
 
-const Buttondesgin = ({buttonoverwriteBackgroundColor,buttonText}) => {
+const Buttondesgin = (props) => {
+
+    console.log(props)
+    const {buttonoverwriteBackgroundColor
+    ,buttonText}=props
+    const array=[6,9,1,2,3,4,5,6]
+    console.log(array)
+    console.log(...array)
+    const [value1,value2,value3,...value4]=array
+    console.log(value1)
+    console.log(value2)
+    console.log(value3)
+    console.log(value4)
+
+
     return (
         <div>
-        <button className="buttonDesign" style={{backgroundColor:buttonoverwriteBackgroundColor}}>{buttonText}</button>
-
+        <button className="buttonDesign" style={{backgroundColor:buttonoverwriteBackgroundColor
+    }}>{buttonText}</button>
+        
         </div>
     )
 }
